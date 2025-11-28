91fe8163f7c33ea5092267110f431d08c45f77ae
NiccoloSicilian <niccolo.siciliano@gmail.com>
2025-11-28 14:05:30 +0100

    Update eval.py

--- src/eval/eval.py
+++ src/eval/eval.py
@@ -151,7 +151,7 @@ def evaluate_task_vector(
     elif args.specify_lambda != "None":
         scaling_coef_range = [args.specify_lambda]
     elif args.method.name in ["iso_c", "iso_cts", "TSVM"]:
-        scaling_coef_range = np.linspace(1.0, 3.0, args.n_eval_points)[1:]
+        scaling_coef_range = np.linspace(1.5, 3.0, args.n_eval_points)[1:]
         print("Evaluation", scaling_coef_range)
     else:
         scaling_coef_range = np.linspace(0.7, 1.0, args.n_eval_points)[1:]
