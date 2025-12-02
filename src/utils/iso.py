57580bcc710b8e532ce88dfc66a1e8bcca6712f7
NiccoloSicilian <niccolo.siciliano@gmail.com>
2025-12-02 13:33:01 +0100

    Update iso.py

--- src/utils/iso.py
+++ src/utils/iso.py
@@ -478,7 +478,7 @@ def dm_whole_vec(task_vectors, config):
 
 def iso_c(task_vectors, config):
     
-    return dm_layer_specific(task_vectors, config)
+    return dm_whole_net_module(task_vectors, config)
 '''
 def new_method(task_vectors, config):
     pretrained_model = ImageEncoder.load(self.model_name, checkpoint)
